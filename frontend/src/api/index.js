a5f0132d65488c460c7b38d0206a4490d8e0c644
Alikhan Baidussenov <alikhanbaidussenov@Noutbuk-Alikhan.local>
2023-11-17 16:46:04 +0600

    IP address changed

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const baseURL = 'http://192.168.1.173:3000/' // http://{ipv4}:8001
+export const baseURL = 'http://192.168.214.17:3000/' // http://{ipv4}:8001
 // export const baseURL = 'http://127.0.0.1:3000/' // http://{ipv4}:8001
 // use ipconfig in cmd
 
