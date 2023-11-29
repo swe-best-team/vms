004bb4c696cf972f251af03e51e74b2393c938d1
Zilola <buyamaya32@gmail.com>
2023-11-29 18:18:48 +0600

    Create a fueling Screen

--- frontend/src/screens/fueler/CreateFuelingScreen.js/index.js
+++ frontend/src/screens/fueler/CreateFuelingScreen.js/index.js
@@ -1,45 +1,109 @@
-import React, { useState } from 'react'
-
-import { View, Text, StyleSheet } from 'react-native'
-
-import Screen from 'components/Screen'
-import VehicleModal from './VehicleModal'
-import Option from 'components/Option'
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Screen from 'components/Screen';
+import VehicleModal from './VehicleModal';
+import Option from 'components/Option';
+import CostModal from './CostModal';
+import VolumeModal from './VolumeModal';
+import StationModal from './StationModal';
 
 const CreateFuelingScreen = () => {
-    const [vehicle, setVehicle] = useState(null)
+    const [vehicle, setVehicle] = useState(null);
+    const [cost, setCost] = useState(null);
+    const [volume, setVolume] = useState(null);
+    const [station, setStation] = useState(null);
 
-    const [modalVisible, setModalVisible] = useState(false)
-    const [vehiclesFound, setVehiclesFound] = useState(false)
+    const [vehicleModalVisible, setVehicleModalVisible] = useState(false);
+    const [costModalVisible, setCostModalVisible] = useState(false);
+    const [volumeModalVisible, setVolumeModalVisible] = useState(false);
+    const [stationModalVisible, setStationModalVisible] = useState(false);
+
+    const [vehiclesFound, setVehiclesFound] = useState(false);
+
+    const handleVehiclePress = () => {
+        if (vehiclesFound) {
+            setVehicleModalVisible(true);
+        }
+    };
+
+    const handleCostPress = () => {
+        setCostModalVisible(true);
+    };
+
+    const handleVolumePress = () => {
+        setVolumeModalVisible(true);
+    };
+
+    const handleStationPress = () => {
+        setStationModalVisible(true);
+    };
+
+    const handleCreateFueling = () => {
+    };
 
     return (
         <Screen>
             <VehicleModal
-                visible={modalVisible}
-                setVisible={setModalVisible}
+                visible={vehicleModalVisible}
+                setVisible={setVehicleModalVisible}
                 setVehicle={setVehicle}
                 setVehiclesFound={setVehiclesFound}
             />
-            <Option
-                style={styles.option}
-                onPress={() => {
-                    if (vehiclesFound)
-                        setModalVisible(true)
-                }}
-            >
+            <CostModal
+                value={cost}
+                setValue={setCost}
+                visible={costModalVisible}
+                setVisible={setCostModalVisible}
+            />
+            <VolumeModal
+                value={volume}
+                setValue={setVolume}
+                visible={volumeModalVisible}
+                setVisible={setVolumeModalVisible}
+            />
+            <StationModal
+                value={station}
+                setValue={setStation}
+                visible={stationModalVisible}
+                setVisible={setStationModalVisible}
+            />
+            <Option style={styles.option} onPress={handleVehiclePress}>
                 {vehiclesFound
                     ? vehicle == null
                         ? 'Choose a vehicle'
                         : `${vehicle.brand} ${vehicle.model}`
-                    : 'No vehicles found'
-                }
+                    : 'No vehicles found'}
+            </Option>
+            <Option style={styles.option} onPress={handleCostPress}>
+                {cost == null ? 'Choose cost' : `Cost: ${cost}`}
+            </Option>
+            <Option style={styles.option} onPress={handleVolumePress}>
+                {volume == null ? 'Choose volume' : `Volume: ${volume}`}
+            </Option>
+            <Option style={styles.option} onPress={handleStationPress}>
+                {station == null ? 'Choose station' : `Station: ${station}`}
+            </Option>
+            <Option style={styles.createButton} onPress={handleCreateFueling}>
+                Create a Fueling
             </Option>
         </Screen>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
+    
 
-})
+    createButton: {
+        marginTop: 20,
+        backgroundColor: '#808080',
+        padding: 15,
+        borderRadius: 5,
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+});
 
 export default CreateFuelingScreen
